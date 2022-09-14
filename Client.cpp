898200056aee70b9bd6e67b57b906960427e6a79
JoelByers <joel.byers@outlook.com>
2022-09-14 11:08:37 -0500

    Add and implement Client.cpp

---
+++ Client.cpp
@@ -0,0 +1,33 @@
+#include <iostream>   
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <cstring>
+using namespace std;
+
+int main(){
+
+    // create socket
+    int socket_description = socket(AF_INET, SOCK_STREAM, 0);
+    if(socket_description == -1){
+        cout << "Unable to create socket" << endl;
+        return 1;
+    }
+
+    struct sockaddr_in server;
+    server.sin_family = inet_addr("0.0.0.0");
+	server.sin_family = AF_INET;
+    server.sin_port = htons(8421);
+
+	if (connect(socket_description , (struct sockaddr *)&server , sizeof(server)) < 0)
+	{
+        cout << "Unable to connect" << endl;
+		return 1;
+	}
+
+    char client_message[3] = {'h','i','\0'};
+    if( send(socket_description , &client_message, strlen(client_message) , 0) < 0)
+	{
+		cout << "Unable to send message to server";
+		return 1;
+	}
+}
